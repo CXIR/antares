038b088ed29ab9260ca1008ffd6c5f17c378d75f
L3Rnumenor <l3r.numenor@icloud.com>
2017-09-19 21:32:45 +0200

    Commit September 19
    Commit September 19 : modification on users route

--- routes/users.js
+++ routes/users.js
@@ -71,36 +71,49 @@ router.get('/:userID',function(req,res){
 router.post('/',function(req,res){
   let send = req.body;
 
-  User.find({
-    where : { 
-              $or : [
-                      { mail       : send.mail },
-                      { identifier : send.identifier }
-                    ]
-            }
+  models.Role.find({
+    where : send.role
   })
-  .then(user => {
-    if(user) res.json({result:0, message:'Similar User w/ same name or same identifier already exists w/ url 05-004'});
-    else{
+  .then(role => {
+    if(role){
 
-        User.create({
-            name       : send.name,
-            first      : send.first,
-            mail       : send.mail,
-            identifier : send.identifier,
-            password   : send.password
-        })
-        .then(user => {
-          if(user){
-            //TODO: affect a Role !
-            res.json({result:0, message:'User successfully created w/ url 05-004'});
-          }
-          else res.json({result:0, message:'User not created w/ url 05-004'});
-        })
-        .catch(err => { res.json({result:-1, message:'Unable to create User w/ url 05-004', error:err}); });
+      User.find({
+        where : { 
+                  $or : [
+                          { mail       : send.mail },
+                          { identifier : send.identifier }
+                        ]
+                }
+      })
+      .then(user => {
+        if(user) res.json({result:0, message:'Similar User w/ same name or same identifier already exists w/ url 05-004'});
+        else{
+
+            User.create({
+                name       : send.name,
+                first      : send.first,
+                mail       : send.mail,
+                identifier : send.identifier,
+                password   : send.password
+            })
+            .then(user => {
+              if(user){
+                user.setRole(role)
+                .then(role => {
+                  res.json({result:1, content:user.responsify()});
+                })
+                .catch(err => { res.json({result:0, message:'Unable to set Role to User w/ url 05-004', error:err}); });
+              }
+              else res.json({result:0, message:'User not created w/ url 05-004'});
+            })
+            .catch(err => { res.json({result:-1, message:'Unable to create User w/ url 05-004', error:err}); });
+        }
+      })
+      .catch(err => { res.json({result:-1, message:'Unable to find User w/ url 05-004', error:err}); });
     }
+    else res.json({result:0, message:'Site not found w/ url 05-004'});
   })
-  .catch(err => { res.json({result:-1, message:'Unable to find User w/ url 05-004', error:err}); });
+  .catch(err => { res.json({result:-1, message:'Unable to find Site w/ url 05-004', error:err}); });
 });
 
 /** Update a User | 05-005 */
@@ -159,9 +172,44 @@ router.post('/mail',function(req,res){
   .catch(err => { res.json({result:-1, message:'Unable to find User w/ url 05-007', error:err}); });
 });
 
+/** Update User Role | 05-008 */
+router.post('/role',function(req,res){
+  let send = req.body;
+
+  models.Role.find({
+    where : {
+              id : send.role
+            }
+  })
+  .then(role => {
+    if(role){
+
+      User.find({
+        where : {
+                  id : send.user
+                }
+      })
+      .then(user =>{
+        if(user){
+
+          user.setRole(role)
+          .then(role => {
+            res.json({result:1, message:'User Role successfully updated w/ url 05-008'});
+          })
+          .catch(err => { res.json({result:-1, message:'Unable to set Role to User w/ url 05-008', error:err}); });
+        }
+        else res.json({result:0, message:'User not found w/ url 05-008'});
+      })
+      .catch(err => { res.json({result:-1, message:'Unable to find User w/ url 05-008', error:err}); });
+    }
+    else res.json({result:0, message:'Role not found w/ url 05-008'});
+  })
+  .catch(err => { res.json({result:-1, message:'Unable to find Role w/ url 05-008', error:err}); });
+});
+
 /******************************* DELETE *****************************/
 
-/** Drop a User | 05-008 */
+/** Drop a User | 05-009 */
 router.delete('/:userID',function(req,res){
 
   User.find({
@@ -174,14 +222,14 @@ router.delete('/:userID',function(req,res){
 
       user.destroy()
       .then(user => {
-        if(user) res.json({result:1, message:'User successfully removed w/ url 05-008'});
-        else res.json({result:0, message:'User not removed w/ url 05-008'});
+        if(user) res.json({result:1, message:'User successfully removed w/ url 05-009'});
+        else res.json({result:0, message:'User not removed w/ url 05-009'});
       })
-      .catch(err => { res.json({result:-1, message:'Unable to remove User w/ url 05-008', error:err}); });
+      .catch(err => { res.json({result:-1, message:'Unable to remove User w/ url 05-009', error:err}); });
     }
-    else res.json({result:0, message:'User not found w/ url 05-008'});
+    else res.json({result:0, message:'User not found w/ url 05-009'});
   })
-  .catch(err => { res.json({result:-1, message:'Unable to find User w/ url 05-008', error:err}); });
+  .catch(err => { res.json({result:-1, message:'Unable to find User w/ url 05-009', error:err}); });
 });
 
 module.exports = router;
